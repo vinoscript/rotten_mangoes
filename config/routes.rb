00908dd75b2023f0e676b8b6541b72a1ae978804
vinoscript <wstikk@gmail.com>
2016-04-26 01:54:46 +0000

    added users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 RottenMangoes::Application.routes.draw do
 
+  get "users/new"
+  get "users/create"
+  
   resources :movies
+
+  resources :users, only: [:new, :create]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426010120) do
+ActiveRecord::Schema.define(version: 20160426014759) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20160426010120) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
 end
