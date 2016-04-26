f66bbde0c1ca1152c67c4db9c23f2791d62dde1e
vinoscript <wstikk@gmail.com>
2016-04-26 02:29:14 +0000

    Added reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 RottenMangoes::Application.routes.draw do
 
+  get "reviews/new"
+  get "reviews/create"
   get "sessions/new"
   get "sessions/create"
   get "users/new"
   get "users/create"
 
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
 
   resources :users, only: [:new, :create]
 
